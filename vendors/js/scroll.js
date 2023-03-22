d07fba49d3104d070bfb52fde30b19014c257289
akeemolushola <iyandakeem@gmail.com>
2023-03-22 01:10:00 +0100

    My First Commit

---
+++ vendors/js/scroll.js
@@ -0,0 +1,21 @@
+import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
+
+const scrollTracker = document.querySelector('.scroll-tracker');
+
+const scrollTrackingTimeline = new ScrollTimeline({
+    source: document.scrollingElement,
+    orientation: 'block',
+    scrollOffsets: [CSS.percent(20), CSS.percent(80)],
+});
+
+scrollTracker.animate(
+    {
+        transform: ['scaleX(0)', 'scaleX(1)']
+    },
+    {
+        duration: 1,
+        timeline: scrollTrackingTimeline,
+    }
+);
+
+
